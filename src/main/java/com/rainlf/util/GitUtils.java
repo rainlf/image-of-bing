8efe074e81655497dbfbfaaf1185f66256a480a0
yhee <yhee@trip.com>
2020-07-10 16:33:29 +0800

    update

--- src/main/java/com/rainlf/util/GitUtils.java
+++ src/main/java/com/rainlf/util/GitUtils.java
@@ -1,20 +1,32 @@
 package com.rainlf.util;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author : rain
  * @date : 2020/7/10 16:06
  */
+@Slf4j
 public class GitUtils {
 
     @SneakyThrows
     public static void commit() {
-        ProcessBuilder pb1 = new ProcessBuilder("git", "commit", "-am", "update");
-        Process process = pb1.start();
-        if (process.exitValue() == 0) {
-            ProcessBuilder pb2 = new ProcessBuilder("git push");
-            pb2.start();
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "commit", "-am", "update", "&&", "git", "push");
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+
+        InputStream inputStream = process.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            log.info("[Git] {}", line);
         }
+
+        process.destroy();
     }
 }
