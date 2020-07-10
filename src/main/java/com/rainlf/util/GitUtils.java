0707ea932e7b9f7f5bc9b1c25ffba256ea457628
yhee <yhee@trip.com>
2020-07-10 16:09:09 +0800

    update

---
+++ src/main/java/com/rainlf/util/GitUtils.java
@@ -0,0 +1,17 @@
+package com.rainlf.util;
+
+import lombok.SneakyThrows;
+
+/**
+ * @author : rain
+ * @date : 2020/7/10 16:06
+ */
+public class GitUtils {
+
+    @SneakyThrows
+    public static void commit() {
+        ProcessBuilder processBuilder = new ProcessBuilder("git ci -am 'update'");
+        Process process = processBuilder.start();
+
+    }
+}
