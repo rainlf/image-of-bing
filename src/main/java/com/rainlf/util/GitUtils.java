e7ddc983d89aa6a3d901ec92d97e769abec9e05c
Rain <iheyu22@163.com>
2020-07-10 22:03:51 +0800

    🍖

--- src/main/java/com/rainlf/util/GitUtils.java
+++ src/main/java/com/rainlf/util/GitUtils.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -14,21 +15,34 @@ import java.io.InputStreamReader;
 @Slf4j
 public class GitUtils {
 
-    @SneakyThrows
+
     public static void commit() {
         log.info("[Git] start commit");
         ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "git add . && git commit -m \uD83C\uDF56 && git push");
         processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
+        Process process = null;
+        try {
+            process = processBuilder.start();
+            InputStream inputStream = process.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-        InputStream inputStream = process.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            log.info("[Git] {}", line);
+            new Thread(() -> {
+                try {
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        log.info("[Git] {}", line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log.info("[Git] end commit");
+            process.destroy();
         }
-        log.info("eeee");
-        process.destroy();
-        log.info("[Git] end commit");
     }
+
 }
