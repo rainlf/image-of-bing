a197b2e3f2d05510907cdc4223efc91f3e1376fe
rain <iheyu22@gmail.com>
2020-12-16 11:22:24 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bing Background
 
-每日2点下载 [https://bing.com](https://bing.com) 背景资源
+每日2点下载 [https://bing.com](https://bing.com) 站点背景图片
 
 ## 构建方式
 ```$xslt
@@ -9,5 +9,5 @@ mvn clean package -DskipTests
 
 ## 运行方式
 ```$xslt
-java -jar target/bing-background-1.0.jar <target_dir>
+java -jar bing-background-store.jar <target_dir>
 ```
---
+++ src/main/java/com/rainlf/Application.java
@@ -0,0 +1,37 @@
+package com.rainlf;
+
+import com.rainlf.service.SpiderService;
+import com.rainlf.service.impl.SpiderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : rain
+ * @date : 2020/7/10 13:37
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        parseArgs(args);
+        SpringApplication.run(Application.class, args);
+    }
+
+    private static void parseArgs(String[] args) {
+        if (args.length == 0) {
+            SpiderServiceImpl.targetDir = "./";
+        } else {
+            SpiderServiceImpl.targetDir = args[0];
+        }
+    }
+
+    @Autowired
+    private SpiderService spiderService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        spiderService.downloadEightDay();
+    }
+}
